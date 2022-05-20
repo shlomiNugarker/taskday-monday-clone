4d803523fd37c94d91b3a9deea045db308e041dc
Shlomi Nugarker <shlomin1231@gmail.com>
2022-05-21 02:26:37 +0300

    to heroku

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -1,4 +1,3 @@
 module.exports = {
-  dbURL:
-    'mongodb+srv://shlomin1231:--password--@cluster0.ysm5t.mongodb.net/taskday_db?retryWrites=true&w=majority',
+  dbURL: `mongodb+srv://shlomin1231:${process.env.DB_TASKDAY}@cluster0.ysm5t.mongodb.net/taskday_db?retryWrites=true&w=majority`,
 }
