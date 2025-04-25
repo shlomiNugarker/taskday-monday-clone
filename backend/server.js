ea1eac980296da508cd809e1a3bacd268cf3dacf
Shlomi Nugarker <shlomin1231@gmail.com>
2025-04-25 13:04:38 +0300

    Update server.js and vite.config.js for production deployment
    - Added production URL to CORS configuration in server.js for enhanced API access.
    - Set base path in vite.config.js to ensure correct asset loading in production environment.

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,8 @@ if (process.env.NODE_ENV === 'production') {
       'http://127.0.0.1:8080',
       'http://localhost:8080',
       'http://127.0.0.1:3000',
-      'http://localhost:3000',
+      'http://localhost:3000', 
+      'https://taskday-monday-production.up.railway.app'
     ],
     credentials: true,
   }
